c31ee9d4c532bf35d618715641fd68bebe51c459
Abhisek Sikdar <abhiseksikdar40@gmail.com>
2025-05-30 00:38:21 +0530

    fix: fix cart.

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const CartSchema = new mongoose.Schema({
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'PlantStore' }
-})
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'PlantStore', required: true },
+    quantity: { type: Number, default: 1, min: 1 }
+});
 
-const Cart = mongoose.model('Cart', CartSchema)
+const Cart = mongoose.model('Cart', CartSchema);
 
-module.exports = Cart
+module.exports = Cart;
